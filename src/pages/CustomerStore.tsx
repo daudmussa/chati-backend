e82db483b4778dee3a08a8529afe2a1a73a58a76
daudmussa <duadarts@gmail.com>
2026-01-18 15:38:55 +0300

    Show 2 products per row on mobile in shop view

--- src/pages/CustomerStore.tsx
+++ src/pages/CustomerStore.tsx
@@ -528,7 +528,7 @@ export default function CustomerStore() {
           </Card>
         ) : (
           <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6">
               {paginatedProducts.map((product) => {
                 const cartQuantity = getCartItemQuantity(product.id);
                 return (
